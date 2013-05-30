0b0b0859c9898f1146425b5923c6de44e2cfd9c9
jamchampnate <nateberrett@gmail.com>
2013-05-30 10:30:03 -0500

    Here is where the submitMaintRequest.php points to for the mySQL variables.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$GLOBALS['mysql_user']  = "";
+$GLOBALS['mysql_pass']	= "";
+$GLOBALS['mysql_db']	= "heritage";
+$GLOBALS['mysql_host']	= "10.0.1.20";
+$GLOBALS['mysql_port']	= "3306";
+$GLOBALS['server_name'] = "10.0.1.16";
+$GLOBALS['root_dir'] = "/opt/cdp/Insite2"
+?>
