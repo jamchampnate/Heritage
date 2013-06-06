947f531e6e5394e2e9416d8e8bca2079ed9b9326
Sean LeFevre <seanlefevre@gmail.com>
2013-06-05 20:47:07 -0600

    Corrected config.php

--- SubmitTask/config.php
+++ SubmitTask/config.php
@@ -1,7 +1,7 @@
 <?php
-$GLOBALS['mysql_user']  = "";
+$GLOBALS['mysql_user']  = "root";
 $GLOBALS['mysql_pass']	= "";
-$GLOBALS['mysql_db']	= "heritage";
+$GLOBALS['mysql_db']	= "maintenancerequest";
 $GLOBALS['mysql_host']	= "127.0.0.1";
 $GLOBALS['mysql_port']	= "3306";
 $GLOBALS['server_name'] = "127.0.0.1";
