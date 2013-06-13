da71898eaaefa143e05725b6a538e1a112d8cd88
Sean LeFevre <seanlefevre@gmail.com>
2013-06-12 20:47:17 -0600

    Submission for corrected.

--- SubmitTask/sendsub.php
+++ SubmitTask/sendsub.php
@@ -26,7 +26,7 @@ $db = new MySQL($GLOBALS['mysql_host'], $GLOBALS['mysql_user'], $GLOBALS['mysql_
 <body>
 
 
-<!//echo results/>
+
 Thank you <?php echo $_POST["name"]; ?>!<br>
 You are at ext <?php echo $_POST["extension"]; ?>.
 Your email is <?php echo $_POST["email"]; ?><br>
@@ -34,10 +34,11 @@ You are having a problem with <?php echo $_POST["issue"]; ?><br>
 You are in <?php echo $_POST["location"]; ?><br>
 It is a <?php echo $_POST["priority"]; ?><br>
 and you described it as:<br><?php echo $_POST["description"]; ?><br>
-and the photo attached was:<br><img src="<?php echo $_POST["photoupload"]; ?>" alt="your upload" />
 
-<!//submit data/>
-<? php
+
+
+<?php
+require_once "config.php";
 $name = $_POST["name"]; 
 $ext = $_POST["extension"];
 $email = $_POST["email"];
@@ -45,16 +46,26 @@ $issue = $_POST["issue"];
 $loc = $_POST["location"];
 $priority = $_POST["priority"];
 $desc = $_POST["description"];
-$photo = $_POST["photoupload"];
+//$photo = $_POST["photoupload"];
 $date = date('Y-m-d H:i:s');
 
-mysql_connect($GLOBALS['mysql_host'],$GLOBALS['mysql_user'],$GLOBALS['mysql_pass']);
-@mysql_select_db($db) or die( "Unable to select database");
+$con=mysqli_connect($GLOBALS['mysql_host'], $GLOBALS['mysql_user'], $GLOBALS['mysql_pass'], $GLOBALS['mysql_db']);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+echo "\nConnected correctly\n";
+$sql="INSERT INTO incident (Priority, Category, Description, Location)
+VALUES('$priority','$issue','$desc','$loc')";
 
-$query = "INSERT INTO incident VALUES ('','$priority','$desc','','$loc','$date', '', '', '$photo')";
-mysql_query($query);
+if (!mysqli_query($con,$sql))
+  {
+  die('Error: ' . mysqli_error($con));
+  }
+echo "1 record added";
 
-mysql_close();
+mysqli_close($con);
 ?>
 <br>
 Thank you for your submission have a nice day!
--- SubmitTask/submitMaintRequest.php
+++ SubmitTask/submitMaintRequest.php
@@ -4,6 +4,8 @@ session_start();
 require_once "config.php";
 require_once "mysql.php";
 $db = new MySQL($GLOBALS['mysql_host'], $GLOBALS['mysql_user'], $GLOBALS['mysql_pass'], $GLOBALS['mysql_db']);
+
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
